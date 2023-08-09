99dea71d23340cb8e27fe7412a3b3e14b6b6ea55
harshal <kardileharshal567@gmail.com>
2023-08-09 12:27:22 +0530

    completed inheritance

--- Inheritance/Shape.php
+++ Inheritance/Shape.php
@@ -1,20 +1,54 @@
 <?php
 
-class Shape{
-    protected $length= 4;
+abstract class Shape{
+    
+    protected $color;
 
-    public function getArea()
+    public function __construct($color='red')
     {
-    return pow($this -> length, 2);
+        $this->color = $color;
     }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    abstract protected function getArea();
 }
 
 class Square extends Shape {
 
+    protected $length = 4;
+
+    public function getArea()
+    {
+        return pow($this-> length, 2);
+    }
 }
 
 class Triangle extends Shape {
+    
+    protected $base = 4;
+    protected $height = 7;
+
+    public function getArea()
+    {
+        return .5 * $this->base * $this->height;
+    }
+}
+
+class Circle extends Shape{
+    
+    protected $radius =5;
+
+    public function getArea(){
+        return M_PI * pow($this ->radius , 2);
+    }
 
 }
 
-echo (new Square)->getArea();
+// echo (new Square('green')) -> getColor();
+// $circle = new Circle;
+// echo $circle->getArea();
+(new Circle) -> getArea();
